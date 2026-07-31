89eb0cc1cca411f940881e01990f6359260d8567
siddharthg-7 <siddharthgoudgilakathi@gmail.com>
2026-07-31 14:02:21 +0530

    feat(web): add premium Glassmorphism FounderOS Navbar component

---
+++ apps/web/app/navbar/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+import { FounderOSNavbar } from '@/components/navigation/FounderOSNavbar';
+
+export default function NavbarPage() {
+  return (
+    <div className="min-h-screen bg-[#0B0F19] text-white">
+      <FounderOSNavbar />
+    </div>
+  );
+}
